32baf5fd23798a925349eeaeded24b6d2f64a798
axonapiwit <axon.apiwit@gmail.com>
2024-05-15 15:50:20 +0700

    improve code

--- src/components/custom/Countdown.tsx
+++ src/components/custom/Countdown.tsx
@@ -1,31 +1,73 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 
-const Countdown = ({}) => {
+const Countdown = () => {
+  const calculateTimeLeft = () => {
+    const endTime = new Date("2024-12-31T00:00:00").getTime();
+    const now = new Date().getTime();
+    const difference = endTime - now;
+
+    let timeLeft = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
       <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
         <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": 15 }}></span>
+          <span
+            style={{ "--value": timeLeft.days } as React.CSSProperties}
+          ></span>
         </span>
         days
       </div>
       <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
         <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": 10 }}></span>
+          <span
+            style={{ "--value": timeLeft.hours } as React.CSSProperties}
+          ></span>
         </span>
         hours
       </div>
       <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
         <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": 24 }}></span>
+          <span
+            style={{ "--value": timeLeft.minutes } as React.CSSProperties}
+          ></span>
         </span>
         min
       </div>
       <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
         <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": 42 }}></span>
+          <span
+            style={{ "--value": timeLeft.seconds } as React.CSSProperties}
+          ></span>
         </span>
         sec
       </div>
